618422bd4a9069c8a36a1e4089779057643c1069
Utilisateur <user@debian.arcahe.ovh>
2017-11-29 14:55:19 +0100

    ajout et modification

--- Projet_IN301/lire_ecrire.c
+++ Projet_IN301/lire_ecrire.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
-
 #include "mes_types.h"
 
 
-SLIDER lire_fichier(char *nom) {
+SLIDER lire_fichier(char *nom) 
+{
 
 	printf("nom = %s\n",nom);
 	SLIDER S;
-	S.L = 10;
-	S.H = 5;
+	FILE fichier = NULL;
+	fichier = fopen(nom, "r");
+	if(fichier != NULL)
+	{
+		fscanf(nom, "%d %d\n", &S.L, &S.H);
+		fscanf(nom, "%d %d\n", &S.xent, &S.yent);
+		fscanf(nom, "%d %d\n", &S.xsor, &S.yent);
+		fscanf(nom, "%d\n", &S.N);
+		for(int i=0; i<S.N; i+)
+		{
+			fscanf(nom "%d %d %d\n", &S.mur->x, &S.mur->y, &S.mur->d);
+			S.mur=S.mur->suiv;
+		}
+		fclose(fichier);
+	}
 	
 	return S;
 }
 
 
-void ecrire_fichier(SLIDER S, char*nom) {
+void ecrire_fichier(SLIDER S, char*nom)
+{
 }
--- Projet_IN301/mes_types.h
+++ Projet_IN301/mes_types.h
@@ -1,8 +1,18 @@
 #define TAILLE_CASE 50
 
+struct elem
+{
+	int x, y, d;
+	struct elem* suiv;
+}
+typedef struct elem LISTE; 
 
-struct slider {
+struct slider
+{
 	int L,H; // Largeur et hauteur de la grille
+	int xent, yent;
+	int xsor, ysor;
+	int N;
+	LISTE mur=NULL;
 };
-
 typedef struct slider SLIDER;
