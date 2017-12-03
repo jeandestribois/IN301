8114ceaf028c824af4f3ebc136e890048e05ddde
Jean Destribois <jean.destribois@gmail.com>
2017-12-03 20:34:45 +0100

    modification

--- Projet_IN301/afficher.c
+++ Projet_IN301/afficher.c
@@ -3,11 +3,26 @@
 #include "mes_types.h"
 
 
-void initialiser_affichage(SLIDER S) {
-	init_graphics (50*S.L,50*S.H);
+void initialiser_affichage(SLIDER S)
+{
+	init_graphics (TAILLE_CASE*S.L,TAILLE_CASE*S.H);
 }
 
-void afficher_grille(SLIDER S) {
+void afficher_grille(SLIDER S)
+{
+	POINT P1, P2;
+	P1.y=0, P2.y=TAILLE_CASE*S.H;
+	for(int i=0; i<S.L; i++)
+	{
+		P1.x=i*TAILLE_CASE, P2.x=P1.x;
+		draw_line(P1,P2,white);
+	}
+	P1.x=0, P2.x=TAILLE_CASE*S.L;
+	for(int i=0; i<S.H; i++)
+	{
+		P1.y=i*TAILLE_CASE, P2.y=P1.y;
+		draw_line(P1,P2,white);
+	}
 }
 
 void afficher_murs(SLIDER S) {
