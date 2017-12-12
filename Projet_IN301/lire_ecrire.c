9df752543f1692573deca490e1c03b81ddfd29e2
Jean Destribois <jean.destribois@gmail.com>
2017-12-12 13:28:35 +0100

    ajout et modification

--- Projet_IN301/lire_ecrire.c
+++ Projet_IN301/lire_ecrire.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "mes_types.h"
 #include "fonctions_liste.h"
 
 SLIDER lire_fichier(char *nom)
 {
 	int x, y, d;
+	char chemin_fichier[100];
 	printf("nom = %s\n", nom);
 	SLIDER S;
 	S.mur = NULL;
 	FILE *fichier = NULL;
-	fichier = fopen(nom, "r");
+	sprintf(chemin_fichier, "dir_slider/%s", nom);
+	fichier = fopen(chemin_fichier, "r");
 	if (fichier != NULL)
 	{
 		fscanf(fichier, "%d %d\n", &S.L, &S.H);
@@ -22,8 +25,13 @@ SLIDER lire_fichier(char *nom)
 			S.mur = ajoute_element_fin(S.mur, x, y, d);
 		}
 		fclose(fichier);
+		return S;
 	}
-	return S;
+	else
+	{
+		fprintf(stderr,"Impossible d'ouvrir le fichier '%s'\n",nom);
+		exit(-1);
+	}
 }
 
 void ecrire_fichier(SLIDER S, char *nom)
